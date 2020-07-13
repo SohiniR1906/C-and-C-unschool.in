08ae30095cf9763877b79199361be029d5ca0a71
SohiniR1906 <68237201+SohiniR1906@users.noreply.github.com>
2020-07-13 20:20:39 +0530

    Add files via upload
    Task 1 : Operators

---
+++ task1operaters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{int a,b,c,d,e,f;
+ printf("Enter first number: ");
+ scanf("%d",&a);
+ printf("Enter second number: ");
+ scanf("%d",&b);
+ c=a+b;
+ printf("The sum of %d and %d is %d",a,b,c);
+ d=a-b;
+ printf("\n The difference of %d and %d is %d",a,b,d);
+ e=a*b;
+ printf("\n The product of %d and %d is %d",a,b,e);
+ f=a/b;
+ printf("\n The division of %d and %d is %d",a,b,f);
+  
+}
